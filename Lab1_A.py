c20ad9336977549cd40655f09f305e95172d0b75
Taher Akolawala <taher.yakolawala@gmail.com>
2024-09-27 17:41:42 -0400

    Add files via upload

---
+++ Lab1_A.py
@@ -0,0 +1,8 @@
+#lab 1 program a
+#converting celsius to fahrenheit
+
+celsius = float(input(f'Enter the temperature in Celsius: ')) #collecting celsius value
+
+fahrenheit = celsius * 9/5 + 32 #calculating celsius to fahrenheit and storing in a var
+
+print(f'That is {fahrenheit:.1f} degrees Fahrenheit!') #output
